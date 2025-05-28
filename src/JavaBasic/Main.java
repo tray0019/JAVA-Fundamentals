34294e78a3247b55e707369b93e2b1a43504ce02
tray0019 <tray0019@algonquinlive.com>
2025-05-28 11:14:03 -0400

    Easy coding, variables, println

---
+++ src/JavaBasic/Main.java
@@ -0,0 +1,35 @@
+/* -- This is a Java Doc to help other programmers understand your code and who wrote them
+ * @author Rustom Trayvilla
+ * @version 1.0
+ * @since 2025-05-24
+ */
+package JavaBasic;
+
+/*
+ * Level: 1
+ * Every class name must start with upper case letter
+ */
+public class Main {
+
+	//This main method is a must for the program to be executed
+	public static void main(String[] args) {
+		
+		//This statement print out Hello World in the console
+		System.out.println("Hello World");
+		
+		//When double quotes are use the value becomes a String
+		System.out.print("Print without line break -!");
+		System.out.println("'println, print with line break'");
+		
+		//Added empty println for a line break
+		System.out.println();
+		
+		//Numeric does not need double quote.
+		System.out.println(64);	
+		System.out.println("Print: "+ 64);
+		//It works with other arithmetic operator 
+		System.out.println(64 + 6);
+	
+	}
+	
+}
---
+++ src/JavaBasic/Variables.java
@@ -0,0 +1,52 @@
+/*
+ * @author Rustom Trayvilla
+ * @version 1.1 
+ * @since 2025-05-25
+ */
+package JavaBasic;
+
+/*
+ * This class will be all about variables
+ * Variables are containers for storing data and values
+ */
+public class Variables {
+	
+	public static void main(String[] args) {
+		
+		//Stores Tom in the name and print it out
+		String name = "Tom";
+		System.out.println("Name: "+ name);
+		
+		//Store 64 on num and print it out
+		int num = 64;
+		System.out.println("Number: "+ num);
+		
+		//Variable can be updated
+		num = 65;
+		System.out.println("New Number: "+ num);
+		
+		//Make sure to have the f at the end.
+		float floatingNum = 64.99f;
+		System.out.println("Float: "+ floatingNum);
+		
+		//Can only store single character
+		char singleLetter = 'f';
+		System.out.println("Char: "+ singleLetter);
+		
+		//Can only store true or false
+		boolean trueFalse = false;
+		System.out.println("Boolean: "+ trueFalse);
+		
+		//The final here makes this variable constant or cannot be updated
+		final int unchangeNum = 64;
+		System.out.println("Unchange Number: "+ unchangeNum);
+		
+		//Adding 
+		int numTwo = 5;
+		int addNum = num + numTwo;
+		System.out.println("Total: "+addNum);
+		
+		
+	}
+
+}
